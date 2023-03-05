0900b1ddce3c7deddaca6cc5b23eba80194c79bb
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-03-05 22:37:13 +0200

    add CORS middleware

--- main.py
+++ main.py
@@ -2,11 +2,25 @@ import redis.asyncio as redis
 from fastapi import FastAPI
 from fastapi_limiter import FastAPILimiter
 from fastapi_limiter.depends import RateLimiter
-
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routes import contacts, auth
 
+
+origins = [ 
+    "http://localhost:3000"
+    ]
+
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router, prefix='/api')
 app.include_router(contacts.router, prefix='/api')

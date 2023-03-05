b40aa8e62f2edf539a50b79f833b198126d49a81
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-03-05 18:16:33 +0200

    add redis user caching

--- src/services/auth.py
+++ src/services/auth.py
@@ -1,5 +1,7 @@
+import pickle
 from typing import Optional
 
+import redis
 from jose import JWTError, jwt
 from fastapi import HTTPException, status, Depends
 from fastapi.security import OAuth2PasswordBearer
@@ -16,6 +18,8 @@ class Auth:
     SECRET_KEY = "secret_key"
     ALGORITHM = "HS256"
     oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
+    
+    redis_cache = redis.Redis(host='localhost', port=6379, db=0)
 
     def verify_password(self, plain_password, hashed_password):
         return self.pwd_context.verify(plain_password, hashed_password)
@@ -86,9 +90,17 @@ class Auth:
         except JWTError as e:
             raise credentials_exception
 
-        user = await repository_users.get_user_by_email(email, db)
+        user = self.redis_cache.get(f'user:{email}')
         if user is None:
-            raise credentials_exception
+            print("USER POSTGRES")
+            user = await repository_users.get_user_by_email(email, db)
+            if user is None:
+                raise credentials_exception
+            self.redis_cache.set(f'user:{email}', pickle.dumps(user))
+            self.redis_cache.expire(f'user:{email}', 900)
+        else:
+            print("USER CACHE")
+            user = pickle.loads(user)
         return user
 
 

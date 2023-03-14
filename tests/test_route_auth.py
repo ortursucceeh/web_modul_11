8657c4d3b65287bb19bec1b77c785e9414ad3551
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2023-03-14 21:42:02 +0200

    add functional tests for routes.auth by using pytest module

---
+++ tests/test_route_auth.py
@@ -0,0 +1,72 @@
+import sys
+import os
+from unittest.mock import MagicMock
+sys.path.append(os.getcwd())
+
+from src.database.models import User
+from src.conf.messages import ALREADY_EXISTS, EMAIL_NOT_CONFIRMED, INVALID_PASSWORD, INVALID_EMAIL
+
+def test_create_user(client, user, monkeypatch):
+    mock_send_email = MagicMock()
+    monkeypatch.setattr("src.routes.auth.send_email", mock_send_email)
+    response = client.post(
+        "/api/auth/signup",
+        json=user,
+    )
+    assert response.status_code == 201, response.text
+    data = response.json()
+    assert data["user"]["email"] == user.get("email")
+    assert "id" in data["user"]
+
+
+def test_repeat_create_user(client, user):
+    response = client.post(
+        "/api/auth/signup",
+        json=user,
+    )
+    assert response.status_code == 409, response.text
+    data = response.json()
+    assert data["detail"] == ALREADY_EXISTS
+
+
+def test_login_user_not_confirmed(client, user):
+    response = client.post(
+        "/api/auth/login",
+        data={"username": user.get('email'), "password": user.get('password')},
+    )
+    assert response.status_code == 401, response.text
+    data = response.json()
+    assert data["detail"] == EMAIL_NOT_CONFIRMED
+
+
+def test_login_user(client, session, user):
+    current_user: User = session.query(User).filter(User.email == user.get('email')).first()
+    current_user.confirmed = True
+    session.commit()
+    response = client.post(
+        "/api/auth/login",
+        data={"username": user.get('email'), "password": user.get('password')},
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["token_type"] == "bearer"
+
+
+def test_login_wrong_password(client, user):
+    response = client.post(
+        "/api/auth/login",
+        data={"username": user.get('email'), "password": 'password'},
+    )
+    assert response.status_code == 401, response.text
+    data = response.json()
+    assert data["detail"] == INVALID_PASSWORD
+
+
+def test_login_wrong_email(client, user):
+    response = client.post(
+        "/api/auth/login",
+        data={"username": 'email', "password": user.get('password')},
+    )
+    assert response.status_code == 401, response.text
+    data = response.json()
+    assert data["detail"] == INVALID_EMAIL
